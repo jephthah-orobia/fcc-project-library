77b649b78c70f84361e1b346e2f46773776a26f9
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2022-12-27 22:38:23 +0800

    Added mongoose connection

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {
+  setDebugging,
+  log,
+  logEr,
+  logPropsOf,
+  logRequest,
+  logQuery,
+  logParams,
+  logBody
+} = require('./log-utils');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+setDebugging(process.env.DEBUG == 'yes');
 
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+app.use(cors({ origin: '*' })); //USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,29 +42,37 @@ app.route('/')
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
+apiRoutes(app);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
-//Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-          console.log('Tests are not valid:');
-          console.error(e);
+mongoose.connect(process.env.MONGO_URI)
+  .then(data => {
+    log('successfully connected to mongodb');
+    //Start our server and tests!
+    const listener = app.listen(process.env.PORT || 3000, function () {
+      console.log('Your app is listening on port ' + listener.address().port);
+      if (process.env.NODE_ENV === 'test') {
+        console.log('Running Tests...');
+        setTimeout(function () {
+          try {
+            runner.run();
+          } catch (e) {
+            console.log('Tests are not valid:');
+            console.error(e);
+          }
+        }, 1500);
       }
-    }, 1500);
-  }
-});
+    });
+  })
+  .catch(e => {
+    logEr('Could not connect to db.')
+    logPropsOf('Error', e);
+  });
 
 module.exports = app; //for unit/functional testing
