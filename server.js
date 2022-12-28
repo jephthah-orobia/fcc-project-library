6a4491009b20538907d96a4189f9ff1b301118eb
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2022-12-28 13:24:38 +0800

    move runner to fcctesting

--- server.js
+++ server.js
@@ -57,17 +57,6 @@ mongoose.connect(process.env.MONGO_URI)
     //Start our server and tests!
     const listener = app.listen(process.env.PORT || 3000, function () {
       console.log('Your app is listening on port ' + listener.address().port);
-      if (process.env.NODE_ENV === 'test') {
-        console.log('Running Tests...');
-        setTimeout(function () {
-          try {
-            runner.run();
-          } catch (e) {
-            console.log('Tests are not valid:');
-            console.error(e);
-          }
-        }, 1500);
-      }
     });
   })
   .catch(e => {
